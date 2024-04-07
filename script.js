d8910e7d94a74fd80e5bcf2183e391a34dd6a0f3
Enio Vrushi <eniovrushi.ev@gmail.com>
2024-04-07 11:02:17 +0200

    Bugfix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (var i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
   const button = document.createElement('button');
   button.innerHTML = `Button number ${i}`;
   button.onclick = function () {
